0f20844853900272428986620a42c9b16ee57edc
johnne <john.sundh@scilifelab.se>
2024-10-16 21:53:16 +0200

    Add more options for outfiles and add cutoffs as arguments

--- clean_asv_data.R
+++ clean_asv_data.R
@@ -15,8 +15,18 @@ parser <- add_option(parser, c("--sample_types"), type="character", default="sam
                     help="Comma-separated list of sample types")
 parser <- add_option(parser, c("--control_types"), type="character", default=c("buffer_blank","pcr_neg","extraction_neg","buffer_blank_art_spikes"),
                     help="Comma-separated list of control types")
-parser <- add_option(parser, c("-o", "--outfile"), type="character", default="cleaned_filtered_counts.tsv",
-                    help="Path to output file")
+parser <- add_option(parser, c("--control_cutoff"), type="numeric", default=0.05,
+                    help="Threshold for removing control clusters. Clusters occurring in more than control_cutoff of control samples will be removed.")
+parser <- add_option(parser, c("--spikein_cutoff"), type="numeric", default=0.8,
+                    help="Threshold for identifying spikein clusters. Clusters occurring in more than spikein_cutoff of spikein samples will be identified as spikeins.")
+parser <- add_option(parser, c("--counts_outfile"), type="character", default="cleaned_filtered_counts.tsv",
+                    help="Path to cleaned filtered counts file")
+parser <- add_option(parser, c("--taxonomy_outfile"), type="character", default="cleaned_filtered_cluster_taxonomy.tsv",
+                    help="Path to cleaned filtered taxonomy file")
+parser <- add_option(parser, c("--control_outfile"), type="character", default=NULL,
+                    help="Path to removed control clusters file")
+parser <- add_option(parser, c("--spikein_outfile"), type="character", default=NULL,
+                    help="Path to removed spikein clusters file")
 
 args <- parse_args(parser)
 args$sample_types <- unlist(strsplit(args$sample_types,","))
@@ -63,11 +73,30 @@ cat(paste0("Reading in taxonomy from ", args$taxonomy, "\n"))
 taxonomy <- read.delim(args$taxonomy)
 taxonomy <- taxonomy[taxonomy$representative==1,]
 
-cleaned_filtered_counts <- remove_control_clusters(filtered_counts,raw_counts,taxonomy,samples,controls)
+res <- remove_control_clusters(filtered_counts,raw_counts,taxonomy,samples,controls, args$control_cutoff)
+cleaned_filtered_counts <- res$counts
+control_remove_tax <- res$remove_tax
 
 if (length(spikein_samples)>0) {
-    cleaned_filtered_counts <- remove_spikes(cleaned_filtered_counts,spikein_samples, taxonomy)
+    res <- remove_spikes(cleaned_filtered_counts,spikein_samples, taxonomy, args$spikein_cutoff)
+    cleaned_filtered_counts <- res$counts
+    spikein_remove_tax <- res$spike_tax
 }
 
-cat(paste0("Writing cleaned filtered counts to ", args$outfile, "\n"))
-write.table(cleaned_filtered_counts,args$outfile,sep="\t",quote=FALSE,row.names=FALSE)
+cleaned_filtered_taxonomy <- taxonomy[taxonomy$cluster %in% cleaned_filtered_counts$cluster,]
+
+cat(paste0("Writing cleaned filtered counts to ", args$counts_outfile, "\n"))
+write.table(cleaned_filtered_counts,args$counts_outfile,sep="\t",quote=FALSE,row.names=FALSE)
+
+cat(paste0("Writing cleaned filtered cluster taxonomy to ", args$taxonomy_outfile, "\n"))
+write.table(cleaned_filtered_taxonomy,args$taxonomy_outfile,sep="\t",quote=FALSE,row.names=FALSE)
+
+if (!is.null(args$control_outfile)) {
+    cat(paste0("Writing removed control clusters to ", args$control_outfile, "\n"))
+    write.table(control_remove_tax,args$control_outfile,sep="\t",quote=FALSE,row.names=FALSE)
+}
+
+if (!is.null(args$spikein_outfile)) {
+    cat(paste0("Writing removed spikein clusters to ", args$spikein_outfile, "\n"))
+    write.table(spikein_remove_tax,args$spikein_outfile,sep="\t",quote=FALSE,row.names=FALSE)
+}
